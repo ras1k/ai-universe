1810fd73a268f3b60afaf7519fc34d8984343308
ras1k <rasikabdullah@gmail.com>
2023-04-01 20:20:14 +0600

    date added

--- js/app.js
+++ js/app.js
@@ -7,8 +7,34 @@ const loadAi = async() =>{
 
 const displayAi = (params) =>{
     console.log(params);
+    const paramContainer = document.getElementById('param-container');
     params.forEach(param =>{
         console.log(param);
+        const paramDiv = document.createElement('div');
+        paramDiv.classList.add('col');
+        paramDiv.innerHTML = `
+        <div class="card h-100">
+            <img src="${param.image}" class="card-img-top img-fluid w-100" alt="...">
+            <div class="card-body">
+            <h3>Features : </h3>
+            <ol>
+                <li>${param.features[0] ? param.features[0] : 'no feature'}</li>
+                <li>${param.features[1] ? param.features[1] : 'no feature'}</li>
+                <li>${param.features[2] ? param.features[2] : 'no feature'}</li>
+                <li>${param.features[3] ? param.features[3] : ''}</li>
+            </ol>
+                
+            </div>
+            <div class="card-footer">
+                <div>
+                    <h5 class="card-title">${param.name}</h5>
+                    <p> <i class="fa-solid fa-calendar"></i> ${param.published_in}</p>
+                </div>
+            </div>
+        </div>
+        `;
+
+        paramContainer.appendChild(paramDiv);
     })
 }
 
