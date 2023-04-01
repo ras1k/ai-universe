f5d1f61ab8f2f24c2c49e429557e5856bda4185f
ras1k <rasikabdullah@gmail.com>
2023-04-01 18:53:44 +0600

    cards added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log('lol')
+const loadAi = async() =>{
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    const res =  await fetch(url);
+    const data = await res.json();
+    displayAi(data.data.tools);
+}
+
+const displayAi = (params) =>{
+    console.log(params);
+    params.forEach(param =>{
+        console.log(param);
+    })
+}
+
+loadAi();
