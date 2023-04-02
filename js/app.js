f76c7dd68d116d00517e8839d6dd30b7b0554944
ras1k <rasikabdullah@gmail.com>
2023-04-02 16:03:07 +0600

    indentation fixed

--- js/app.js
+++ js/app.js
@@ -26,18 +26,17 @@ const displayAi = (params, dataLimit) =>{
                 <li>${param.features[0] ? param.features[0] : ''}</li>
                 <li>${param.features[1] ? param.features[1] : ''}</li>
                 <li>${param.features[2] ? param.features[2] : ''}</li>
-            </ol>
-                
+            </ol> 
             </div>
             <div class="card-footer">
                 <div class="d-flex align-items-center justify-content-between">
-                <div>
-                    <h5 class="card-title">${param.name? param.name : 'No Name Found'}</h5>
-                    <p> <i class="fa-solid fa-calendar"></i> ${param.published_in? param.published_in : 'No Date Found'}</p>
-                </div>
-                <button onclick="loadData(${param.id})" type="button" class="btn" style="background-color: rgba(165, 79, 79, 0.308); border: 1px; border-radius: 50%; padding: 15px" data-bs-toggle="modal" data-bs-target="#dataModal">
-                <i class="fa-solid fa-arrow-right" style="color: #b60202;"></i>
-                </button>
+                    <div>
+                        <h5 class="card-title">${param.name? param.name : 'No Name Found'}</h5>
+                        <p> <i class="fa-solid fa-calendar"></i> ${param.published_in? param.published_in : 'No Date Found'}</p>
+                    </div>
+                    <button onclick="loadData(${param.id})" type="button" class="btn" style="background-color: rgba(165, 79, 79, 0.308); border: 1px; border-radius: 50%; padding: 15px" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                        <i class="fa-solid fa-arrow-right" style="color: #b60202;"></i>
+                    </button>
                 </div>
             </div>
         </div>`;
@@ -80,55 +79,54 @@ const displayDataDetails = (param) =>{
     document.getElementById('dataModalLabel').innerText = param.tool_name;
     document.getElementById('data-details').innerHTML = `
     <div class="d-md-flex gap-5 justify-content-center">
-                <div class="border border-danger-subtle rounded bg-danger-subtle bg-opacity-25 p-2">
-                        <p class="fw-bold">${param.description}
-                        </p>
-                        <div class="row d-md-flex gap-2 p-3 text-center flex-md-nowrap flex-lg-nowrap justify-content-center">
-                            <div class="col bg-light rounded">
-                                <p class="text-success fw-bold p-5">
-                                ${param.pricing[0].price} /
-                                ${param.pricing[0].plan}</p>
-                            </div>
-                            <div class="col bg-light rounded">
-                                <p class="text-warning fw-bold p-5">
-                                ${param.pricing[1].price} /
-                                ${param.pricing[1].plan}</p>
-                            </div>
-                            <div class="col bg-light rounded">
-                                <p class="text-danger fw-bold p-5">
-                                ${param.pricing[2].price} /
-                                ${param.pricing[2].plan}</p></p>
-                            </div>
-                        </div>
-                        <div class="row d-flex g-3">
-                            <div class="col rounded">
-                            <h3 class="fw-bold">Features</h3>
-                                <ul>
-                                    <li class ="text-secondary">${param.features["1"].feature_name}</li>
-                                    <li class ="text-secondary">${param.features["2"].feature_name}</li>
-                                    <li class ="text-secondary">${param.features["3"].feature_name}</li>
-                                </ul>
-                            </div>
-                            <div class="col rounded">
-                            <h3 class="fw-bold">Integrations</h3>
-                                <ul>
-                                    <li class ="text-secondary">${param.integrations[0]}</li>
-                                    <li class ="text-secondary">${param.integrations[1]}</li>
-                                    <li class ="text-secondary">${param.integrations[2]}</li>
-                                    <li class ="text-secondary">${param.integrations[3] ? param.integrations[3] : '' }</li>
-                                </ul>
-                            </div>
-                        </div>
+        <div class="border border-danger-subtle rounded bg-danger-subtle bg-opacity-25 p-2">
+            <p class="fw-bold">${param.description}</p>
+            <div class="row d-md-flex gap-2 p-3 text-center flex-md-nowrap flex-lg-nowrap justify-content-center">
+                <div class="col bg-light rounded">
+                    <p class="text-success fw-bold p-5">
+                    ${param.pricing[0].price ? param.pricing[0].price : 'No Price Found'} /
+                    ${param.pricing[0].plan ? param.pricing[0].price : 'No Plan Found'}</p>
                 </div>
-                <div>
-                    <img src="${param.image_link[0]}" class="mb-5 w-100" alt="">
-                    <button class="btn btn-danger position-absolute top-0 end-0 m-4 d-none d-lg-block d-md-block">${param.accuracy['score']? param.accuracy['score'] : ''}% accuracy</button>
-                    <div class="text-center">
-                        <p class="fw-bold mb-2">${param.input_output_examples[0].input}</p>
-                        <p class="text-secondary">${param.input_output_examples[0].output}</p>
-                    </div>
+                <div class="col bg-light rounded">
+                    <p class="text-warning fw-bold p-5">
+                    ${param.pricing[1].price} /
+                    ${param.pricing[1].plan}</p>
                 </div>
+                <div class="col bg-light rounded">
+                    <p class="text-danger fw-bold p-5">
+                    ${param.pricing[2].price} /
+                    ${param.pricing[2].plan}</p></p>
+                </div>
+        </div>
+        <div class="row d-flex g-3">
+            <div class="col rounded">
+                <h3 class="fw-bold">Features</h3>
+                <ul>
+                    <li class ="text-secondary">${param.features["1"].feature_name}</li>
+                    <li class ="text-secondary">${param.features["2"].feature_name}</li>
+                    <li class ="text-secondary">${param.features["3"].feature_name}</li>
+                </ul>
             </div>
+            <div class="col rounded">
+                <h3 class="fw-bold">Integrations</h3>
+                <ul>
+                    <li class ="text-secondary">${param.integrations[0]}</li>
+                    <li class ="text-secondary">${param.integrations[1]}</li>
+                    <li class ="text-secondary">${param.integrations[2]}</li>
+                    <li class ="text-secondary">${param.integrations[3] ? param.integrations[3] : '' }</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+        <div>
+            <img src="${param.image_link[0]}" class="mb-5 w-100" alt="">
+            <button class="btn btn-danger position-absolute top-0 end-0 m-4 d-none d-lg-block d-md-block">${param.accuracy['score']? param.accuracy['score'] : ''}% accuracy</button>
+            <div class="text-center">
+                <p class="fw-bold mb-2">${param.input_output_examples[0].input}</p>
+                <p class="text-secondary">${param.input_output_examples[0].output}</p>
+            </div>
+        </div>
+    </div>
     `
 }
 
